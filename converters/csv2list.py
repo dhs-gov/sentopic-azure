2d7801b11dc61188578ac3277c4ed5ab0b096c20
StephenQuirolgico <stephen.quirolgico@gmail.com>
2021-03-13 19:20:52 -0500

    Switched to io.ByteIO with wrapper to avoid non UTF-8 char errors.

--- converters/csv2list.py
+++ converters/csv2list.py
@@ -1,8 +1,12 @@
 import csv
+import io
 from io import StringIO
+from io import BytesIO
 import nltk
 from globals import globalvars
+from globals import globalutils
 from . import data_cleaner
+import pandas as pd
 
 class CsvRow:
     def __init__(self, row_num, text):
@@ -11,11 +15,15 @@ class CsvRow:
 
 
 def get_data(csv_bytes):
-    string_data = StringIO(csv_bytes.decode())
-    reader = csv.reader(string_data, quotechar='"', delimiter=',')
+    fh_bytes = io.BytesIO(csv_bytes)
+    fh = io.TextIOWrapper(fh_bytes, encoding='cp1252', errors='strict')
+    reader = fh.readlines()
+    #string_data = StringIO(csv_bytes.decode())
+    #reader = csv.reader(df1, quotechar='"', delimiter=',')
     data = []
     for row in reader:
         text = "".join(row)
+        #print("text: ", text)
         # Make sure text is not over max tokens
         if len(nltk.word_tokenize(text)) > globalvars.MAX_TOKENS:
             text = globalutils.get_last_sentence(text)
