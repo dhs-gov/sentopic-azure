931a21760e07c97d0050fe4dc2a9aff38b2fad7d
StephenQuirolgico <stephen.quirolgico@gmail.com>
2021-03-14 12:37:46 -0400

    Refactored classes.

--- sentopic_activity/__init__.py
+++ sentopic_activity/__init__.py
@@ -28,11 +28,14 @@ class Paragraph:
 
 
 class Topic:
-    def __init__(self, topic_num, word, weight):
+    def __init__(self, topic_num, word_weight):
         self.topic_num = topic_num
+        self.word_weight = word_weight
+
+class Word:
+    def __init__(self, word, weight):
         self.word = word
         self.weight = weight
-
 
 # Here, 'name' is the incoming data_in payload.
 def main(name: object) -> json:
@@ -43,79 +46,71 @@ def main(name: object) -> json:
     classifier = EasySequenceClassifier()
     class3_sentiment_rows = sentiment_3class.assess(classifier, data_list)
     star5_sentiment_rows = sentiment_5star.assess(classifier, data_list)
-    print("Memory 1")
-    print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    print("Memory 1: ", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
 
     # Get topic numbers for each row, and words per topic
-    bert_sentence_topics, bert_topics = topic_mod_bertopic.get_topics(data_list)
-    lda_sentences_topics, lda_topics = topic_mod_lda.get_topics(data_list, 4)
-    print("Memory 2")
+    bert_sentence_topics, bert_topics, bert_error = topic_mod_bertopic.get_topics(data_list)
+    print("Num bert topics: ", len(bert_topics))
+
+    lda_sentence_topics, lda_topics, lda_error = topic_mod_lda.get_topics(data_list, 4)
+    print("Num lda topics: ", len(lda_topics))
+
+    print("Memory 2: ", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
     print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
 
     # Print topic distribution
-    print("BERT Topic Distribution: ", Counter(bert_sentence_topics))
-    print("LDA Topic Distribution: ", Counter(lda_sentences_topics))
+    if not bert_error:
+        print("BERT Topic Distribution: ", Counter(bert_sentence_topics))
+    if not lda_error:
+        print("LDA Topic Distribution: ", Counter(lda_sentence_topics))
 
     # Create results data
     results = []
     fields = ['Text', 'BERT Topic', 'LDA Topic', '3-Class Sentiment', '5-Star Sentiment']
     for i in range(len(data_list)):
+        bert_topic = None
+        if bert_sentence_topics:
+            bert_topic = bert_sentence_topics[i]
+        lda_topic = None
+        if lda_sentence_topics:
+            lda_topic = lda_sentence_topics[i]
+
         if (data_list[i]):
             paragraph = Paragraph(
                 data_list[i],
-                bert_sentence_topics[i],
-                lda_sentences_topics[i],
+                bert_topic,
+                lda_topic,
                 class3_sentiment_rows[i].sentiment,
                 star5_sentiment_rows[i].sentiment
             )
             results.append(paragraph)
     
     # Create BERT topics data
-    bert_topics = []
-    #bert_out = filename + "_bert_topics_out.csv"
-    fields = ['BERT Topic', 'Word', 'Weight']
+    bert_topics_list = []
+    words_list = []
+    print("LDA size: ", )
     for i in range(len(bert_topics)):
+        topic = Topic(bert_topics[i].topic_num, None)
         for j in range(len(bert_topics[i].words)):
-            topic = Topic(
-                bert_topics[i].topic_num,
-                bert_topics[i].words[j],
-                bert_topics[i].weights[j]
-            )
-            bert_topics.append(topic)
-
-
-    # Create BERT topics table
-    #with open(bert_out, 'w', newline='', encoding='cp850', errors='ignore') as csvfile:
-    #    csvwriter = csv.writer(csvfile, delimiter='^')
-    #    csvwriter.writerow(fields)
-    #    csvwriter.writerows(rows)
-
+            word = Word(bert_topics[i].words[j], bert_topics[i].weights[j])
+            words_list.append(word)
+        topic.word_weight = words_list
+        bert_topics_list.append(topic.word_weight)
 
     # Create LDA topics data
-    #lda_out = filename + "_lda_topics_out.csv"
-    lda_topics = []
-    fields = ['LDA Topic', 'Word', 'Weight']
+    lda_topics_list = []
+    words_list = []
     print("LDA size: ", )
     for i in range(len(lda_topics)):
+        topic = Topic(lda_topics[i].topic_num, None)
         for j in range(len(lda_topics[i].words)):
-            topic = Topic(
-                lda_topics[i].topic_num,
-                lda_topics[i].words[j],
-                lda_topics[i].weights[j]
-            )
+            word = Word(lda_topics[i].words[j], lda_topics[i].weights[j])
+            words_list.append(word)
+        topic.word_weight = words_list
+        lda_topics_list.append(topic.word_weight)
 
-            lda_topics.append(topic)
 
-    # Create LDA topics table
-    #with open(lda_out, 'w', newline='', encoding='cp850', errors='ignore') as csvfile:
-    #    csvwriter = csv.writer(csvfile, delimiter='^')
-    #    csvwriter.writerow(fields)
-    #    csvwriter.writerows(rows)
-
-    print("berts len: ", len(bert_topics))
-    print("lda len: ", len(lda_topics))
-
-    r = Result(results, bert_topics, lda_topics)
+    r = Result(results, bert_topics_list, lda_topics_list)
 
     json_out = jsonpickle.encode(r, unpicklable=False)
     #print("JSON STR OUT: ", json_out)
